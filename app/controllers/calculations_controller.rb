50d582c444147caa11c310041d63063ba587b36e
hxie0 <xhy@chicagobooth.edu>
2021-08-05 19:25:16 +0000

    cookies completed!

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -18,8 +18,11 @@ class CalculationsController < ApplicationController
   def add
     @first_number = params.fetch("first_num").to_f
     @second_number = params.fetch("second_num").to_f
-
     @result = @first_number.to_f + @second_number.to_f
+
+    cookies.store(:addition_first, @first_number)
+    cookies.store(:addition_second, @second_number)
+    cookies.store(:addition_result, @result)
 
     render({ :template => "calculation_templates/add_results.html.erb" })
   end
@@ -27,8 +30,11 @@ class CalculationsController < ApplicationController
   def subtract
     @first_number = params.fetch("first_num").to_f
     @second_number = params.fetch("second_num").to_f
-
     @result =  @second_number.to_f - @first_number.to_f
+
+    cookies.store(:subtraction_first, @first_number)
+    cookies.store(:subtraction_second, @second_number)
+    cookies.store(:subtraction_result, @result)
 
     render({ :template => "calculation_templates/subtract_results.html.erb" })
   end
@@ -36,8 +42,11 @@ class CalculationsController < ApplicationController
   def multiply
     @first_number = params.fetch("first_num").to_f
     @second_number = params.fetch("second_num").to_f
-
     @result =  @first_number.to_f * @second_number.to_f
+
+    cookies.store(:multiplication_first, @first_number)
+    cookies.store(:multiplication_second, @second_number)
+    cookies.store(:multiplication_result, @result)
 
     render({ :template => "calculation_templates/multiply_results.html.erb" })
   end
@@ -45,8 +54,11 @@ class CalculationsController < ApplicationController
   def divide
     @first_number = params.fetch("first_num").to_f
     @second_number = params.fetch("second_num").to_f
-
     @result =  @first_number.to_f / @second_number.to_f
+
+    cookies.store(:division_first, @first_number)
+    cookies.store(:division_second, @second_number)
+    cookies.store(:division_result, @result)
 
     render({ :template => "calculation_templates/divide_results.html.erb" })
   end
